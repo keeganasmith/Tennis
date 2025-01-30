cac55aaf56acbc9a09a7f78d100945b0d77aa0ee
Keegan Smith <keeganasmith2003@gmail.com>
2025-01-30 14:44:29 -0600

    cleanup

--- neural_net.py
+++ neural_net.py
@@ -1,3 +1,4 @@
+from typing import OrderedDict
 import pandas as pd
 import os
 import torch
@@ -7,23 +8,23 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import numpy as np
 import joblib
+def create_layers(num_layers, num_params_per_layer, input_size, num_classes):
+    result = OrderedDict()
+    result["layer0"] = nn.Linear(input_size, num_params_per_layer)
+    result["relu0"] = nn.ReLU()
+    for i in range(1, num_layers + 1):
+        result[f"layer{i}"] = nn.Linear(num_params_per_layer, num_params_per_layer)
+        result[f"relu{i}"] = nn.ReLU()
+    result[f"output"] = nn.Linear(num_params_per_layer, num_classes)
+    return result
+
 class NeuralNetwork(nn.Module):
     def __init__(self, input_size, num_classes):
         super(NeuralNetwork, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(input_size, 512),
-            nn.ReLU(),  
-            nn.Linear(512, 512),
-            nn.ReLU(), 
-            nn.Linear(512, 512),
-            nn.ReLU(),  
-            nn.Linear(512, 512),  
-            nn.ReLU(),  
-            nn.Linear(512, 512),  
-            nn.ReLU(),  
-            nn.Linear(512, num_classes)  # Output layer, no activation (logits)
-        )
+        num_params_per_layer = 512
+        self.layers = nn.Sequential(create_layers(5, num_params_per_layer, input_size, num_classes))
 
     def forward(self, x):
         return self.layers(x)
@@ -33,7 +34,7 @@ def train_nn(df):
     df = df.sample(frac=1, random_state=42).reset_index(drop=True)
     train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
     train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
-
+    
     # Prepare training data
     X_train = train_df.drop(columns=["player1_won"]).values
     Y_train = train_df["player1_won"].values
@@ -59,7 +60,7 @@ def train_nn(df):
     # Initialize the model, loss function, optimizer, and scheduler
     model = NeuralNetwork(input_size=len(df.columns) - 1, num_classes=2)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, factor=0.5)
 
     num_epochs = 20
@@ -67,30 +68,47 @@ def train_nn(df):
         # Training step
         model.train()
         train_loss = 0
+        train_correct = 0
+        train_total = 0
         for batch_features, batch_labels in train_dataloader:
             outputs = model(batch_features)
             loss = criterion(outputs, batch_labels)
             train_loss += loss.item()
 
+            # Calculate training accuracy
+            _, predicted = torch.max(outputs, 1)  # Get the predicted class
+            train_correct += (predicted == batch_labels).sum().item()
+            train_total += batch_labels.size(0)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         train_loss /= len(train_dataloader)  # Average training loss for the epoch
+        train_accuracy = train_correct / train_total  # Training accuracy for the epoch
 
-        # Validation step (compute validation loss)
+        # Validation step (compute validation loss and accuracy)
         model.eval()
         val_loss = 0
+        val_correct = 0
+        val_total = 0
         with torch.no_grad():
             for val_features, val_labels in val_dataloader:
                 val_outputs = model(val_features)
                 val_loss += criterion(val_outputs, val_labels).item()
+
+                # Calculate validation accuracy
+                _, val_predicted = torch.max(val_outputs, 1)
+                val_correct += (val_predicted == val_labels).sum().item()
+                val_total += val_labels.size(0)
         val_loss /= len(val_dataloader)  # Average validation loss for the epoch
+        val_accuracy = val_correct / val_total  # Validation accuracy for the epoch
 
         # Adjust learning rate based on validation loss
-        
         scheduler.step(val_loss)
         print("New learning rate: ", scheduler.get_last_lr())
-        print(f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
+        print(f"Epoch [{epoch+1}/{num_epochs}], "
+            f"Training Loss: {train_loss:.4f}, Training Accuracy: {train_accuracy:.4f}, "
+            f"Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}")
 
     # Evaluate the model on the test set
     model.eval()
@@ -105,6 +123,7 @@ def train_nn(df):
     
 def main():
     df = joblib.load("processed_df.pkl")   
+    print(df.isin([np.inf, -np.inf]).values.sum()) 
     train_nn(df)
 if __name__ == "__main__":
     main()
--- prepare_data.py
+++ prepare_data.py
@@ -4,8 +4,22 @@ import joblib
 import pandas as pd
 import numpy as np
 from datetime import datetime
-def compute_service_percentage_per_game_prefix(match_df, num_years):
-    # Convert tourney_date to datetime if not already
+def reverse_columns(columns):
+    column_dict = {}
+    for i in range(0, len(columns)):
+        new_column_name = None
+        if(columns[i] == "player1_won"):
+            continue
+        if(columns[i][:7] == "player1"):
+            new_column_name = "player2" + columns[i][7:]
+        elif(columns[i][:7] == "player2"):
+            new_column_name = "player1" + columns[i][7:]
+        else:
+            continue
+        column_dict[columns[i]] = new_column_name
+    print(column_dict)
+    return column_dict
+def retrieve_player_stats(match_df, num_years):
     match_df["tourney_date"] = pd.to_datetime(match_df["tourney_date"], format="%Y%m%d")
 
     # Sort by tourney_date
@@ -18,6 +32,7 @@ def compute_service_percentage_per_game_prefix(match_df, num_years):
         "match_id": match_df["match_id"].tolist() + match_df["match_id"].tolist(),  # Use match_id for merging
         "tourney_date": match_df["tourney_date"].tolist() + match_df["tourney_date"].tolist(),
         "serve_pct": match_df["w_serve_pct"].tolist() + match_df["l_serve_pct"].tolist(),
+        "bp_saved_pct": match_df["w_bp_save_pct"].tolist() + match_df["l_bp_save_pct"].tolist(),
         "is_winner": [1] * len(match_df) + [0] * len(match_df)
     })
 
@@ -28,11 +43,16 @@ def compute_service_percentage_per_game_prefix(match_df, num_years):
     # Add columns for the rolling sum and count within the last num_years
     player_stats["rolling_serve_pct_sum"] = 0.0
     player_stats["rolling_match_count"] = 0
+    player_stats["matches_won"] = 0
+
+    player_stats["rolling_bp_pct_sum"] = 0.0
     print("SLIDING WINDOW")
     # Use a sliding window to calculate the rolling stats
     for player_id, group in player_stats.groupby("player_id"):
         serve_pct_sum = 0
         match_count = 0
+        matches_won = 0
+        bp_pct_sum = 0
         j = 0  # Sliding window pointer
 
         for i in range(len(group)):
@@ -42,15 +62,20 @@ def compute_service_percentage_per_game_prefix(match_df, num_years):
             # Slide the window to exclude matches older than num_years
             while j < i and group.iloc[j]["tourney_date"] < cutoff_date:
                 serve_pct_sum -= group.iloc[j]["serve_pct"]
+                bp_pct_sum -= group.iloc[j]["bp_saved_pct"]
+                matches_won -= group.iloc[j]["is_winner"]
                 match_count -= 1
                 j += 1
 
             # Assign rolling sum and count (excluding current match)
             player_stats.loc[group.index[i], "rolling_serve_pct_sum"] = serve_pct_sum
             player_stats.loc[group.index[i], "rolling_match_count"] = match_count
-
+            player_stats.loc[group.index[i], "matches_won"] = matches_won
+            player_stats.loc[group.index[i], "rolling_bp_pct_sum"] = bp_pct_sum
             # Update rolling sum and count for the next iteration (excluding current match)
             serve_pct_sum += group.iloc[i]["serve_pct"]
+            bp_pct_sum += group.iloc[i]["bp_saved_pct"]
+            matches_won += group.iloc[i]["is_winner"]
             match_count += 1
 
     # Calculate rolling averages
@@ -58,41 +83,62 @@ def compute_service_percentage_per_game_prefix(match_df, num_years):
     player_stats["rolling_avg_serve_pct"] = (
         player_stats["rolling_serve_pct_sum"] / player_stats["rolling_match_count"]
     ).fillna(0)
-    # Merge rolling averages back into the original DataFrame using match_id
-    print("MERGING ROLLING AVERAGES")
-    # Merge for winners
+    player_stats["rolling_avg_bp_pct"] = (
+        player_stats["rolling_bp_pct_sum"] / player_stats["rolling_match_count"]
+    ).fillna(0)
+    new_stat_columns = ["rolling_avg_serve_pct", "rolling_avg_bp_pct", "rolling_match_count", "matches_won"]
+    return player_stats, new_stat_columns
+
+def get_rename_mapping(original_columns, prefix):
+    result = {}
+    for column in original_columns:
+        result[column] = prefix +"_" + column
+    return result
+def merge_dataframes(match_df, other_df, columns_to_merge_on, rename_mapping, is_winner):
     match_df = match_df.merge(
-        player_stats[player_stats["is_winner"] == 1][["match_id", "rolling_avg_serve_pct"]],
+        other_df[other_df["is_winner"] == is_winner][columns_to_merge_on],
         left_on="match_id",
         right_on="match_id",
         how="left",
         suffixes=("", "_winner")
-    ).rename(columns={"rolling_avg_serve_pct": "w_avg_serve_pct"})
-
+    ).rename(columns=rename_mapping)
+    return match_df
+def compute_service_percentage_per_game_prefix(match_df, num_years):
+    player_stats, new_stats_columns = retrieve_player_stats(match_df, num_years)    
+    # Merge rolling averages back into the original DataFrame using match_id
+    columns_to_merge_on = new_stats_columns + ["match_id"]
+    winner_rename_mapping = get_rename_mapping(new_stats_columns, "w")
+    loser_rename_mapping = get_rename_mapping(new_stats_columns, "l")
+    print("MERGING ROLLING AVERAGES")
+    # Merge for winners
+    match_df = merge_dataframes(match_df, player_stats, columns_to_merge_on, winner_rename_mapping, 1)
+    print("winner rename mappings: ", winner_rename_mapping)
+    print("loser rename mappings: ", loser_rename_mapping)
     # Merge for losers
-    match_df = match_df.merge(
-        player_stats[player_stats["is_winner"] == 0][["match_id", "rolling_avg_serve_pct"]],
-        left_on="match_id",
-        right_on="match_id",
-        how="left",
-        suffixes=("", "_loser")
-    ).rename(columns={"rolling_avg_serve_pct": "l_avg_serve_pct"})
-
+    match_df = merge_dataframes(match_df, player_stats, columns_to_merge_on, loser_rename_mapping, 0)
     # Fill NaN values with 0 (for new players or missing data)
     # match_df["w_avg_serve_pct"] = match_df["w_avg_serve_pct"].fillna(0)
     # match_df["l_avg_serve_pct"] = match_df["l_avg_serve_pct"].fillna(0)
-
+    
     return match_df
+
+def compute_surface_stats(match_df, num_years):
+    new_df = match_df.copy()
+    for surface_type, group in match_df.groupby("surface"):
+        player_stats, stats_columns = retrieve_player_stats(group, num_years)
+        winner_rename_mapping = get_rename_mapping(stats_columns, "w_" + surface_type + "_")
+        loser_rename_mapping = get_rename_mapping(stats_columns, "l_" + surface_type + "_")
+
+        
+
 def label_data(match_df):
     match_df.loc[match_df["player2_entry"] == "Alt", "player2_entry"] = "ALT"
     match_df.loc[match_df["player1_entry"] == "Alt", "player1_entry"] = "ALT"
     columns_to_encode = [
         "player1_entry",
         "player1_hand",
-        "player1_ioc",
         "player2_entry",
         "player2_hand",
-        "player2_ioc",
         "surface",
         "tourney_level"
     ]
@@ -110,7 +156,7 @@ def label_data(match_df):
     return encoded_df
     
 def handle_na(match_df):
-    
+    #print(match_df.isna().sum())
     match_df["player1_entry"] = match_df["player1_entry"].fillna("O")
     match_df["player2_entry"] = match_df["player2_entry"].fillna("O")
     match_df["player1_age"] = match_df["player1_age"].fillna(26.15)
@@ -130,11 +176,37 @@ def drop_cols(match_df):
         "w_ace", "w_df", "w_svpt", "w_1stIn", "w_1stWon", "w_2ndWon", "w_SvGms", "w_bpSaved", "w_bpFaced",
         "l_ace", "l_df", "l_svpt", "l_1stIn", "l_1stWon", "l_2ndWon", "l_SvGms", "l_bpSaved", "l_bpFaced",
         "tourney_id", "tourney_date", "winner_seed", "loser_seed", "winner_rank_points", "loser_rank_points",
-        "round", "minutes", "w_serve_pct", "l_serve_pct", "match_id", "tourney_name"
+        "round", "minutes", "w_serve_pct", "l_serve_pct", "match_id", "tourney_name", "w_bp_save_pct", "l_bp_save_pct",
+        "winner_ioc", "loser_ioc"
     ]
     match_df = match_df.drop(columns = drop_columns)
     return match_df
+
+def check_for_correct_columns(match_df, reversed_df):
+    for i in range(0, len(match_df.columns)):
+        if(not match_df.columns[i] in reversed_df.columns):
+            print("column ", match_df.columns[i], " not in reversed df")
+    for i in range(0, len(reversed_df.columns)):
+        if(not reversed_df.columns[i] in match_df.columns):
+            print("column ", reversed_df.columns[i], " not in match df")
     
+def pretty_print_columns(match_df):
+    for column in match_df.columns:
+        print(column)
+
+def rename_mapping_for_conversion_to_player(columns):
+    result = {}
+    for column in columns:
+        if(column[0:2] == "w_"):
+            result[column] = "player1_" + column[2:]
+        elif(column[0:2] == "l_"):
+            result[column] = "player2_" + column[2:]
+        elif(column[0:7] == "winner_"):
+            result[column] = "player1_" + column[7:]
+        elif(column[0:6] == "loser_"):
+            result[column] = "player2_" + column[6:]
+    return result
+
 def prepare_data():
     # Load the match and player DataFrames
     match_df = joblib.load(MATCH_DF_PATH)
@@ -150,61 +222,71 @@ def prepare_data():
     match_df["w_serve_pct"] = match_df["w_serve_pct"].fillna(0.6)
     match_df["l_serve_pct"] = match_df["l_serve_pct"].fillna(0.6)
     
+    match_df["w_bp_save_pct"] = (match_df["w_bpSaved"] / match_df["w_bpFaced"])
+    match_df["l_bp_save_pct"] = (match_df["l_bpSaved"] / match_df["l_bpFaced"])
+    match_df.loc[match_df["w_bpSaved"] == 0, "w_bp_save_pct"] = .5
+    match_df.loc[match_df["l_bpSaved"] == 0, "l_bp_save_pct"] = .5
+
     print("COMPUTING SERVE WIN PERCENTAGES")
     match_df = compute_service_percentage_per_game_prefix(match_df, 3)
+    #print(match_df.columns)
     match_df = drop_cols(match_df)
-
+    
     # Rename columns to player1/player2 format
-    rename_mapping = {
-        "winner_entry": "player1_entry",
-        "winner_hand": "player1_hand",
-        "winner_ht": "player1_ht",
-        "winner_ioc": "player1_ioc",
-        "winner_age": "player1_age",
-        "winner_rank": "player1_rank",
 
-        "loser_entry": "player2_entry",
-        "loser_hand": "player2_hand",
-        "loser_ht": "player2_ht",
-        "loser_ioc": "player2_ioc",
-        "loser_age": "player2_age",
-        "loser_rank": "player2_rank",
-        
-        "w_avg_serve_pct": "player1_avg_serve_pct",
-        "l_avg_serve_pct": "player2_avg_serve_pct"
-    }
+    # rename_mapping = {
+    #     "winner_entry": "player1_entry",
+    #     "winner_hand": "player1_hand",
+    #     "winner_ht": "player1_ht",
+    #     "winner_age": "player1_age",
+    #     "winner_rank": "player1_rank",
+
+    #     "loser_entry": "player2_entry",
+    #     "loser_hand": "player2_hand",
+    #     "loser_ht": "player2_ht",
+    #     "loser_age": "player2_age",
+    #     "loser_rank": "player2_rank",
+
+    #     "w_avg_serve_pct": "player1_avg_serve_pct",
+    #     "l_avg_serve_pct": "player2_avg_serve_pct",
+    #     "w_avg_bp_pct": "player1_avg_bp_pct",
+    #     "l_avg_bp_pct": "player2_avg_bp_pct",
+    #     "w_matches_played": "player1_matches_played",
+    #     "l_matches_played": "player2_matches_played",
+    #     "w_matches_won": "player1_matches_won",
+    #     "l_matches_won": "player2_matches_won"
+    # }
+    rename_mapping = rename_mapping_for_conversion_to_player(match_df.columns)
     match_df = match_df.rename(columns=rename_mapping)
-    match_df["player1_won"] = 1  # Set the winner label for the original dataset
+    match_df["player1_won"] = 1  
 
     # Handle missing values and label data
     match_df = handle_na(match_df)
     match_df = label_data(match_df)
-
+    match_df["player1_entry_S"] = 0
+    #print(match_df.head())
     # Create the reversed DataFrame (switch player1 and player2)
-    reversed_df = match_df.rename(columns={
-        "player1_entry": "player2_entry",
-        "player1_hand": "player2_hand",
-        "player1_ht": "player2_ht",
-        "player1_ioc": "player2_ioc",
-        "player1_age": "player2_age",
-        "player1_rank": "player2_rank",
-        "player1_avg_serve_pct": "player2_avg_serve_pct",
-        "player2_entry": "player1_entry",
-        "player2_hand": "player1_hand",
-        "player2_ht": "player1_ht",
-        "player2_ioc": "player1_ioc",
-        "player2_age": "player1_age",
-        "player2_rank": "player1_rank",
-        "player2_avg_serve_pct": "player1_avg_serve_pct",
-    })
+    reversed_columns = reverse_columns(match_df.columns)
+    reversed_df = match_df.rename(columns=reversed_columns)
 
+    #print(reversed_df.head())
+    # _zero_entries = len(match_df.loc[match_df["player1_avg_bp_pct"] != 0])
+    # num_non_zero_entries_l = len(match_df.loc[match_df["player2_avg_bp_pct"] != 0])
+    # print("num non zero entries (1): ", num_non_zero_entries)
+    # print("num non zero entries (2): ", num_non_zero_entries_lnum_non)
+    # print("max of player1_avg_bp_pct: ", reversed_df["player1_avg_bp_pct"].max())
+    # print("min of player1_avg_bp_pct: ", reversed_df["player1_avg_bp_pct"].min())
     reversed_df["player1_won"] = 0  # Reverse the label for reversed matches
     # Concatenate the original and reversed datasets
+    check_for_correct_columns(match_df, reversed_df)
     doubled_df = pd.concat([match_df, reversed_df], ignore_index=True)
 
     print(f"Original dataset size: {len(match_df)}")
     print(f"Doubled dataset size: {len(doubled_df)}")
-
+    #print(doubled_df.isin([np.inf, -np.inf]).values.sum()) 
+    #print(doubled_df.isna().sum())
+    print("columns:")
+    pretty_print_columns(doubled_df)
     return doubled_df
 
 def main():
